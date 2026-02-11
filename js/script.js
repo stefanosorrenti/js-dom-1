a90ec1c49eb68867809fcdbf4105546ed50f528e
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-11 16:15:28 +0100

    add comment

--- js/script.js
+++ js/script.js
@@ -4,16 +4,22 @@ const lampIMG = document.querySelector('img');
 
 const lampButton = document.getElementById('button-lamp')
 
+const buttonText = lampButton.innerText;
+
 
 
 
 //CREO L'EVENTO
-
+//quando lampButton (la nostra img) viene cliccata
 lampButton.addEventListener('click', function(){
     
-    console.log('cliccato');
+    //la src dell'immagine cambia
     lampIMG.src = './img/yellow_lamp.png'
+    //e il testo del bottono diventa spegni
     lampButton.innerText = 'SPEGNI'
     
     
 })
+
+
+
